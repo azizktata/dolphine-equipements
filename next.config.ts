6235346ba4c7d6dd45110aaa8d0a5c85e153ff32
azizktata <65952489+azizktata@users.noreply.github.com>
2024-12-07 09:31:06 +0100

    images domain config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['dolphiv.cluster030.hosting.ovh.net'], // Add your image domain
+  },
 };
 
 export default nextConfig;
